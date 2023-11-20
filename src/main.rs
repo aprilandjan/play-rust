c83aa3fdf5fe1e23b5ffc74749e2daaae72e9989
aprilandjan <merlin.ye@qq.com>
2023-11-20 23:57:19 +0800

    feat: add code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use ferris_says::say;
 use std::io::{stdout, BufWriter};
 
+// "import * as lifetimes from './lifetimes'" equivalent in rust
+mod lifetimes;
+
 fn main() {
     println!("Hello, world!");
 
@@ -13,4 +16,6 @@ fn main() {
     // mut: mutable variable, pointer or sth like that?...
     let mut writer = BufWriter::new(stdout.lock());
     say(message.as_bytes(), width + 20, &mut writer).unwrap();
+
+    lifetimes::test_lifetimes()
 }
