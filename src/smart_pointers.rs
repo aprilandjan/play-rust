b576012218fb6040190b54ce4dabd2bba69ead26
aprilandjan <merlin.ye@qq.com>
2023-11-29 23:49:14 +0800

    feat: add code snippets

--- src/smart_pointers.rs
+++ src/smart_pointers.rs
@@ -36,3 +36,56 @@ pub fn drop_manually() {
   println!("b dropped manually");
   println!("going to leave...");
 }
+
+//==================
+
+// this is just a interface for someone to implement
+pub trait Messenger {
+  fn send(&self, msg: &str);
+}
+
+pub struct LimitTracker<'a, T: Messenger> {
+  messenger: &'a T,
+  value: usize,
+  max: usize,
+}
+
+impl<'a, T> LimitTracker<'a, T> where T:Messenger {
+  pub fn new(messenger: &T, max: usize) -> LimitTracker<T> {
+    LimitTracker { messenger, value: 0, max }
+  }
+  pub fn set_value(&mut self, value: usize) {
+    self.value = value;
+    let percentage_of_max = self.value as f64 / self.max as f64;
+    if percentage_of_max >= 1.0 {
+      self.messenger.send("Error: all of your quota are consumed!");
+    } else if percentage_of_max >= 0.9 {
+      self.messenger.send("Warning: over 90% of your quota has been consumed!");
+    } else if percentage_of_max >= 0.75 {
+      self.messenger.send("Info: over 75% of your quota has been consumed...")
+    }
+  }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Messenger;
+
+  struct MockMessenger {
+    sent_messages: Vec<String>,
+  }
+
+  impl MockMessenger {
+    fn new() -> MockMessenger {
+      MockMessenger { sent_messages: vec![] }
+    }
+  }
+
+  // this is the actual struct which implement the 'Messenger' trait
+  impl Messenger for MockMessenger {
+    fn send(&self, msg: &str) {
+      // now we wll need to mutate self, so `send` in `Messenger` trait won't fit
+      // self.sent_messages.push(msg.to_owned())
+    }
+  }
+}
