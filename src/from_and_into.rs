7582f5c9a04cfce427c09ec50454f69cdf8bb23f
aprilandjan <merlin.ye@qq.com>
2024-01-02 23:24:52 +0800

    feat: add code snippets

--- src/from_and_into.rs
+++ src/from_and_into.rs
@@ -33,12 +33,31 @@ impl From<Address> for Location {
   }
 }
 
+// impl Into<Location> for Address {
+//   fn into(self) -> Location {
+//     let city = match self.city {
+//       City::Beijing => "Beijing",
+//       City::Shanghai => "Shanghai",
+//       City::Guangzhou => "Guangzhou",
+//       City::Shenzhen => "Shenzhen"
+//     };
+//     Location {
+//       details: (city.to_owned() + " city, " + &self.street + " street"),
+//     }
+//   }
+// }
+
 pub fn convert_address_to_location() {
   let address = Address {
     city: City::Shenzhen,
     street: "Nanhai avenue".to_owned(),
   };
   println!("address before convert: {:?}", address);
-  let location = Location::from(address);
-  println!("location from the address: {:?}", location);
+  // let location = Location::from(address);
+  // println!("location from the address: {:?}", location);
+
+  // the 'into' is automatically available if we had 'from' trait defined
+  // but we need to explicitly declare typing
+  let location: Location = address.into();
+  println!("address into location: {:?}", location);
 }
