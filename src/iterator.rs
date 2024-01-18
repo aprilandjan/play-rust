118cadde652ae80210201714b2d73f852944745f
merlin.ye <merlin.ye@qq.com>
2024-01-18 22:29:17 +0800

    feat: add code snippet

--- src/iterator.rs
+++ src/iterator.rs
@@ -1,10 +1,14 @@
 pub fn filter_numeric_value_in_vec() {
-  let list: Vec<u32> = vec![0, 1, 2, 3, 4, 5];
+  let a: Vec<u32> = vec![0, 1, 2, 3, 4, 5];
 
   // here, `into_iter` gets the reference of the numeric element
   // so we need to use `deref` operator to get the value
-  let a: Vec<u32> = list.into_iter().filter(|v| *v != 0).collect();
-  println!("a: {:?}", a);
+  let b: Vec<u32> = list.into_iter().filter(|v| *v != 0).collect();
+  println!("filtered list: {:?}", b);
+
+  // besides, `into_iter` takes ownership of the vec
+  // so the original vec cannot be used anymore
+  // println!("original list: {:?}", a);
 
   // TODO: difference between `into_iter` and `iter`?
 }
