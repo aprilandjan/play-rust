98631ba8322f3bd64a5d3a54f269f04bd25e8f11
merlin.ye <merlin.ye@qq.com>
2024-01-26 22:08:19 +0800

    feat: add code snippets

--- src/lessons/6_try_enum.rs
+++ src/lessons/6_try_enum.rs
@@ -1,15 +1,21 @@
 use rand::Rng;
 
 #[derive(Debug)]
-struct A {
+struct T {
   x: u32,
+}
+
+#[derive(Debug, Clone)]
+enum Z {
+  ZA = 1,
+  ZB = 2,
 }
 
 // three possible enum syntaxes
 // https://stackoverflow.com/questions/50775023/why-do-i-get-an-error-when-pattern-matching-a-struct-like-enum-variant-with-fiel
 #[derive(Debug)]
 pub enum Data {
-  A(A),
+  A(T),
   B(u32, u32),
   C {
     width: u32,
@@ -18,22 +24,32 @@ pub enum Data {
   D {
     m: u32,
     n: u32,
-  }
+  },
+  E(Z),
 }
 
 fn generate_random_structured_enum_value() -> Data {
   let r = rand::thread_rng().gen_range(0..10);
-  if r < 3 {
-    Data::A(A {
+  if r < 2 {
+    Data::A(T {
       x: 100,
     })
-  } else if r < 6 {
+  } else if r < 4 {
     Data::B(100, 200)
-  } else {
+  } else if r < 6 {
     Data::C {
       width: 100,
       height: 200,
     }
+  } else if r < 8 {
+    Data::D {
+      m: 3,
+      n: 2,
+    }
+  } else if r < 9 {
+    Data::E(Z::ZA)
+  } else {
+    Data::E(Z::ZB)
   }
 }
 
@@ -45,6 +61,8 @@ pub fn output_random_enum_structured_enum_value() -> Data {
     Data::B(w, h) => {println!("Data B matched, w={:?}, height={:?}", w, h)},
     Data::C{width, height} => {println!("Data C matched, width={:?}, height={:?}", width, height)},
     Data::D {..} => {println!("Data D matched, and dont care about it")},
+    Data::E(Z::ZA) => {println!("Data E with ZA matched")},
+    Data::E(Z::ZB) => {println!("Data E with ZB matched")},
   };
   return r;
 }
